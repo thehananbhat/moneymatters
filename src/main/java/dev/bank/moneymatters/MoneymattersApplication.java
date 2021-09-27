9cff23d9c7062f44888c3bf3566f98ba89a78237
Mohammad Hanan Bhat <mobhat@cisco.com>
2021-09-27 17:37:55 +0530

    Initial Set-up

---
+++ src/main/java/dev/bank/moneymatters/MoneymattersApplication.java
@@ -0,0 +1,13 @@
+package dev.bank.moneymatters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneymattersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneymattersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/bank/moneymatters/MoneymattersApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bank.moneymatters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneymattersApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
