328aa4296859a50944ec47db8b116ea133721222
Mohammad Hanan Bhat <mobhat@cisco.com>
2021-09-28 16:20:52 +0530

    Autowired AccoutRepo

--- src/main/java/dev/bank/moneymatters/services/ManagerService.java
+++ src/main/java/dev/bank/moneymatters/services/ManagerService.java
@@ -2,6 +2,8 @@ package dev.bank.moneymatters.services;
 
 import dev.bank.moneymatters.entities.Account;
 import dev.bank.moneymatters.entities.Customer;
+import dev.bank.moneymatters.entities.Transaction;
+import dev.bank.moneymatters.repositories.AccountRepo;
 import dev.bank.moneymatters.repositories.CustomerRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 @Service
@@ -16,6 +19,10 @@ public class ManagerService {
 
     @Autowired
     private CustomerRepo cust;
+
+    @Autowired
+    private AccountRepo repo;
+
     public Customer createNewCustomer(Customer newCust) {
         try {
             newCust = cust.save(newCust);
@@ -42,13 +49,9 @@ public class ManagerService {
         System.out.println(account.getCurrentBalance());
 
         try {
-
             account = repo.save(account);
-
         }catch(Exception e) {
-
             throw e;
-
         }
 
         return account;
